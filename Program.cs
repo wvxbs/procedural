c003d7a3cc698931b6a2678262b39e04d594023f
wvxbs <gabriel.ferreira7854@gmail.com>
2020-05-12 19:06:08 -0300

    json added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newstonsoft.json;
 
 namespace procedural
 {
@@ -11,21 +14,89 @@ namespace procedural
 
         static void getUserInput()
         {
-            while(true)
+            int select = 0;
+            Console.WriteLine("1 para operações com rot13\n2 para operações com cifra");
+            if(int.TryParse(Console.ReadLine(), out select))
             {
-                ResolveCripto("'12k'12j1jcj1jjs'");
+                processUserInput(select);
+            }
+            else
+            {
+                Error("tipo de entrada errado");
             }
         }
 
-        static void ResolveCripto (string key) 
+        static void processUserInput(int select)
         {
-            Console.WriteLine(key);
+            int i;
+
+            while(i == 0)
+            {
+                if(select == 1)
+                {
+                    Ex1();
+                }
+                else
+                if(select == 2)
+                {
+                    Ex2();
+                }
+                else
+                if(select == 0)
+                {
+                    i++;
+                }
+                else
+                {
+                    Error("tipo de enrada inválido");
+                }
+            }
         }
 
-        static void ScrambleText() 
+        static void Ex1 ()
+        {
+            int select = 0;
+
+            Console.WriteLine("1 para criptografar texto\n2 para descriptografar texto\n0 para sair");
+            if(int.TryParse(Console.ReadLine(), out select))
+            {
+                        
+            }
+        }
+
+        static void Ex2 ()
+        {
+
+        }
+
+        static void ProcessRot13()
         {
             
         }
-        
+
+        static void loadJson()
+            RootObject ro = new RootObject();
+                try
+                {
+
+                    StreamReader sr = new StreamReader(FileLoc);
+                    string jsonString = sr.ReadToEnd();
+                    JavaScriptSerializer ser = new JavaScriptSerializer();
+                    ro = ser.Deserialize<RootObject>(jsonString);
+
+
+            }
+        }
+
+        public class RootObject 
+        {
+            string characte;
+            int index;
+        }
+
+        static void Error(string error)
+        {
+            Console.WriteLine($"Erro: {error}");
+        }
     }
 }
